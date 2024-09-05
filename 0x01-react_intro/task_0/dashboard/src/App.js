901ecb8bea289b362811321c02d099f17387c7c1
Muhammad Agboola <mk.agbola@gmail.com>
2024-09-06 00:51:10 +0100

    task-0

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,11 +1,13 @@
-import logo from 'Holberton-logo.jpg';
+import logo from './Holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className='App-header'>
+    <div className='App'>
+      <div className='App-header'>
       <img src={logo} alt='Holberton' />
       <h1>School dashboard</h1>
+      </div>
       <div className='App-body'>
         <p>Login to access the full dashboard</p>
       </div>
