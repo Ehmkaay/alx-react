77c19b963151acb27086a910eeaed66de41c2f56
Muhammad Agboola <mk.agbola@gmail.com>
2024-09-06 18:24:43 +0100

    delete

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import './Notifications.css'
-import { Button } from 'react-bootstrap';
-import { getLatestNotification } from './utils';
+import './Notifications.css';
 import closeIcon from './close-icon.png';
+import { getLatestNotification } from './utils';
 
 const Notifications = () => {
-    return (
-        <div className='Notifications'>
-            <p style={{ display: 'inline', marginRight: '80% '}}>
-                Here is the list of notifications
-            </p>
-            <Button
-                aria-label='Close'
-                onClick={console.log('Close button has been clicked')}
-            >
-                <img style={{ display: 'inline' }} src={closeIcon} alt='Close'/>
-            </Button>
-            <ul>
-                <li data='default'>New course available</li>
-                <li data='urgent'>New resume available</li>
-                <li
-                        data='urgent'
-                        dangerouslySetInnerHTML={{ __html: getLatestNotification}}
-                ></li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className='Notifications'>
+			<p style={{ display: 'inline', marginRight: '80%' }}>
+				Here is the list of notifications
+			</p>
+			<button
+				aria-label='Close'
+				onClick={console.log('Close button has been clicked')}
+			>
+				<img style={{ display: 'inline' }} src={closeIcon} alt='Close' />
+			</button>
+			<ul>
+				<li data='default'>New course available</li>
+				<li data='urgent'>New resume available</li>
+				<li
+					data='urgent'
+					dangerouslySetInnerHTML={{ __html: getLatestNotification( )}}
+				></li>
+			</ul>
+		</div>
+	);
 };
 
 export default Notifications;
